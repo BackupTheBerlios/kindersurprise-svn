b0082fed13e0050c2f63b7ccc032579f25d9d126
susanoo <susanoo@942943e2-7a02-4422-866e-44a6b3dd8b14>
2011-01-26 21:35:03 +0000

    Bug #17882 deleted nonsense script alter_table_tFigur_addConstraint fixed the wrong constraint in tPicture

--- Scripts/20100828_2210_alter_table_tPicture_addConstraint.sql
+++ Scripts/20100828_2210_alter_table_tPicture_addConstraint.sql
@@ -1,6 +1,8 @@
+ALTER TABLE tPicture ADD FK_Instructions_ID int;
+
 ALTER TABLE tPicture
-	ADD CONSTRAINT FK_Figur_Instructions FOREIGN KEY (FK_Figur_ID)
-	REFERENCES tFigur (FigurId);
+	ADD CONSTRAINT FK_Picture_Instructions FOREIGN KEY (FK_Instructions_ID)
+	REFERENCES tInstructions (Id);
 
 INSERT INTO CHECK_UPDATES(CREATION_DATE, SCRIPT_NAME, SCRIPT_PURPOSE)
 VALUES(NOW(), '20100828_2210_alter_table_tPicture_addtInstructions', 'alter table tPicture to save also Instructionspictures in the database');
--- Scripts/20100828_2218_alter_table_tFigur_addConstraint.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE tPicture ADD FK_Instructions_ID int;
-
-ALTER TABLE tPicture
-	ADD CONSTRAINT FK_Instructions_Picture FOREIGN KEY (FK_Instructions_ID)
-	REFERENCES tInstructions (Id);
-
